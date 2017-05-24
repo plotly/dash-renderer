6f16cbc4593621f5b1055708262b2dc80981373b
chriddyp <chris@plot.ly>
2017-05-24 14:15:36 -0400

    Webpack config for a new local index point
    This new index will serve as our playground for making a client-side
    version of the app

---
+++ config/partials/outputLocal.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var path = require('path');
+var partial = require('webpack-partial').default;
+
+var ROOT = process.cwd();
+var BUILD = path.join(ROOT, 'dash_renderer');
+
+module.exports = function (config) {
+    return partial(config, {
+        output: {
+            path: BUILD,
+            publicPath: '/dash_renderer/',
+            // TODO: Bundle filename should be hashed (#10)
+            filename: 'local.js'
+        }
+    });
+};
