3564574a0efcb6ab7c875ba4eaba4451665ae0f7
Valentijn Nieman <valentijnnieman@gmail.com>
2018-09-14 10:49:48 -0400

    Replace typeof with Ramda's type

--- src/actions/index.js
+++ src/actions/index.js
@@ -472,7 +472,7 @@ function updateOutput(
     }
 
     if(hooks.request_pre !== null) {
-        if(typeof hooks.request_pre === 'function') {
+        if(type(hooks.request_pre) === 'function') {
             hooks.request_pre(payload);
         }
         else {
@@ -607,7 +607,7 @@ function updateOutput(
 
             // Fire custom request_post hook if any
             if(hooks.request_post !== null) {
-                if(typeof hooks.request_post === 'function') {
+                if(type(hooks.request_post) === 'function') {
                     hooks.request_post(payload, data.response);
                 }
                 else {
