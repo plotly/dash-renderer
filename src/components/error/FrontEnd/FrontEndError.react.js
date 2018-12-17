67dec7502c916d02a9fd0ebe512e15b75b344204
Valentijn Nieman <valentijnnieman@gmail.com>
2018-12-17 14:56:38 -0500

    List for front end errors

--- src/components/error/FrontEnd/FrontEndError.react.js
+++ src/components/error/FrontEnd/FrontEndError.react.js
@@ -1,49 +1,92 @@
 import './FrontEndError.css';
+import {Component} from 'react';
 import ErrorIcon from '../icons/ErrorIcon.svg';
 import CloseIcon from '../icons/CloseIcon.svg';
+import CollapseIcon from '../icons/CollapseIcon.svg';
 import PropTypes from 'prop-types';
 import InlineSVG from 'svg-inline-react';
 
-const FrontEndError = ({e, resolve, inAlertsTray}) => {
-    let closeButton, cardClasses;
-    // if resolve is defined, the error should be a standalone card
-    if(resolve) {
-        closeButton = <InlineSVG className="dash-fe-error__icon-close" src={CloseIcon} onClick={() => resolve('frontEnd', e.myUID)}/>
-        cardClasses = 'dash-error-card';
-    } else {
-        cardClasses = 'dash-error-card__content';
+class FrontEndError extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: this.props.isListItem,
+        };
     }
-    if(inAlertsTray) {
-        cardClasses += ' dash-error-card--alerts-tray'
-    }
-    window.console.log('e', e)
-    return (
-        <div className={cardClasses}>
-            <div className="dash-fe-error-top">
-                <InlineSVG className="dash-fe-error__icon-error" src={ErrorIcon} />
+
+    render() {
+        const {e, resolve, inAlertsTray} = this.props;
+        const {collapsed} = this.state;
+
+        let closeButton, cardClasses;
+        // if resolve is defined, the error should be a standalone card
+        if (resolve) {
+            closeButton = (
+                <InlineSVG
+                    className="dash-fe-error__icon-close"
+                    src={CloseIcon}
+                    onClick={() => resolve('frontEnd', e.myUID)}
+                />
+            );
+            cardClasses = 'dash-error-card';
+        } else {
+            cardClasses = 'dash-error-card__content';
+        }
+        if (inAlertsTray) {
+            cardClasses += ' dash-error-card--alerts-tray';
+        }
+        return collapsed ? (
+            <div className="dash-error-card__list-item">
+                <InlineSVG
+                    className="dash-fe-error__icon-error"
+                    src={ErrorIcon}
+                />
                 <h6 className="dash-fe-error__title">
                     {e.error.message ||
                         'An error was thrown that was not an Error object, so info could not be gathered.'}
                 </h6>
-                {closeButton}
+                <InlineSVG
+                    className="dash-fe-error__collapse"
+                    src={CollapseIcon}
+                    onClick={() => this.setState({collapsed: false})}
+                />
             </div>
-            <p />
-            <div className="dash-fe-error__st">
-                {e.error.stack &&
-                    e.error.stack.split('\n').map(line => <p>{line}</p>)}
+        ) : (
+            <div className={cardClasses}>
+                <div className="dash-fe-error-top">
+                    <InlineSVG
+                        className="dash-fe-error__icon-error"
+                        src={ErrorIcon}
+                    />
+                    <h6 className="dash-fe-error__title">
+                        {e.error.message ||
+                            'An error was thrown that was not an Error object, so info could not be gathered.'}
+                    </h6>
+                    {this.props.isListItem ? <InlineSVG
+                        className="dash-fe-error__collapse dash-fe-error__collapse--flipped"
+                        src={CollapseIcon}
+                        onClick={() => this.setState({collapsed: true})}
+                    /> : closeButton}
+                </div>
+                <div className="dash-fe-error__st">
+                    {e.error.stack &&
+                        e.error.stack.split('\n').map(line => <p>{line}</p>)}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 FrontEndError.propTypes = {
     e: PropTypes.object,
     resolve: PropTypes.func,
-    inAlertsTray: PropTypes.bool
+    inAlertsTray: PropTypes.bool,
+    isListItem: PropTypes.bool,
 };
 
 FrontEndError.defaultProps = {
-    inAlertsTray: false
-}
+    inAlertsTray: false,
+    isListItem: false,
+};
 
 export {FrontEndError};
--- src/components/error/FrontEnd/FrontEndErrorContainer.js
+++ src/components/error/FrontEnd/FrontEndErrorContainer.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import './FrontEndError.css';
 import CloseIcon from '../icons/CloseIcon.svg';
-import LeftArrow from '../icons/LeftArrow.svg';
-import RightArrow from '../icons/RightArrow.svg';
 import PropTypes from 'prop-types';
 import InlineSVG from 'svg-inline-react';
 import {FrontEndError} from './FrontEndError.react';
@@ -10,67 +8,36 @@ import {FrontEndError} from './FrontEndError.react';
 class FrontEndErrorContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selected: 0,
-        };
-        this.nextError = this.nextError.bind(this);
-        this.previousError = this.previousError.bind(this);
-    }
-
-    nextError() {
-        const selected = this.state.selected;
-        const max = this.props.errors.length-1;
-
-        if(selected < max) {
-            this.setState({
-                selected: this.state.selected + 1
-            })
-        }
-    }
-    previousError() {
-        const selected = this.state.selected;
-
-        if(selected > 0) {
-            this.setState({
-                selected: this.state.selected - 1
-            })
-        }
     }
 
     render() {
-        const currentError = this.props.errors[this.state.selected];
         const errorsLength = this.props.errors.length;
         const inAlertsTray = this.props.inAlertsTray;
-        let cardClasses = "dash-error-card dash-error-card--container";
-        if(inAlertsTray) {
+        let cardClasses = 'dash-error-card dash-error-card--container';
+
+        const errorElements = this.props.errors.map(error => {
+            return <FrontEndError e={error} isListItem={true} />;
+        });
+        if (inAlertsTray) {
             cardClasses += ' dash-error-card--alerts-tray';
         }
         return (
             <div className={cardClasses}>
                 <div className="dash-error-card__topbar">
-                    <InlineSVG
-                        className="dash-fe-error__icon-arrow"
-                        src={LeftArrow}
-                        onClick={this.previousError}
-                    />
                     <h6 className="dash-error-card__message">
-                        New Alerts ({this.state.selected + 1} of {errorsLength})
+                        New Alerts ({errorsLength} errors)
                     </h6>
-                    <InlineSVG
-                        className="dash-fe-error__icon-arrow"
-                        src={RightArrow}
-                        onClick={this.nextError}
-                    />
                     <InlineSVG
                         className="dash-fe-error__icon-close"
                         src={CloseIcon}
                         onClick={() =>
-                            this.props.resolve('frontEnd', currentError.myUID)
+                            this.props.errors.forEach(error => {
+                                this.props.resolve('frontEnd', error.myUID);
+                            })
                         }
                     />
                 </div>
-                <p />
-                <FrontEndError e={currentError} />
+                <div className="dash-error-card__list">{errorElements}</div>
             </div>
         );
     }
@@ -79,11 +46,11 @@ class FrontEndErrorContainer extends Component {
 FrontEndErrorContainer.propTypes = {
     errors: PropTypes.array,
     resolve: PropTypes.func,
-    inAlertsTray: PropTypes.bool
+    inAlertsTray: PropTypes.bool,
 };
 
 FrontEndErrorContainer.propTypes = {
-    inAlertsTray: false
-}
+    inAlertsTray: false,
+};
 
 export {FrontEndErrorContainer};
